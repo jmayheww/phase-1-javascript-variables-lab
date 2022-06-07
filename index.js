06f1120b40a94ce19f274a395a2af99a3d01c7ee
Joshua <joshsmayhew@gmail.com>
2022-06-07 15:02:23 +0700

    commit tree ca2afb313a76347964670ed74ba375a19707404b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+
+function letVariable (variable) {
+    let companyName = "Scuber";
+    return companyName;
+}
+
+letVariable;
+
+const companyName = "Scuber";
+
+function moreVariables(variables) {
+    const mostProfitableNeighborhood = "Chelsea";
+    const companyCeo = "Susan Smith";
+    return mostProfitableNeighborhood, companyCeo;
+
+};
+
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
